e3666ed86daaeaf6b111e426b74edcbec66564de
VCamp <vini.camp1907@gmail.com>
2023-09-02 10:32:06 -0300

    Projeto JWT Backend

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post("/sign", (req, res) => {
-  const email = "dener@vidafullstack.com.br";
+  const email = "vinicius@email.com.br";
   const password = "123";
 
   if (req.body.email === email && req.body.password === password) {
     const data = {
-      nome: "Dener Troquatte",
+      nome: "Vinicius",
       email,
       role: ["sysAdmin"],
     };
